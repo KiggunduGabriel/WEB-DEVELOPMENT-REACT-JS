daf10d085b7d308caa10488b2f43c756f4f16f49
KiggunduGabriel <gabrielkiggundu04@gmail.com>
2024-10-23 09:52:50 +0300

    React apps

---
+++ my_app/my_react/src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+function Home() {
+  return<div>
+    <hi>KFC APPLICATION</hi>
+    <p>This is a very nice application</p>
+  </div>
+}
+export default Home;
+ 
