c468e0c87eb18a1e85c1a8398e420993dd9aa924
KiggunduGabriel <gabrielkiggundu04@gmail.com>
2024-11-07 18:03:48 +0300

    Add files via upload

---
+++ mydb.js
@@ -0,0 +1,10 @@
+//this is the interface in which all the data will be stored. like lists, name 
+
+
+let students=[
+    {'id':1,'name':'Elijah'},
+    {'id':2,'name':'Gundu'},
+    {'id':3,'name':'Aaron'},
+    {'id':4,'name':'Geoff'},
+    {'id':5,'name':'Love'},
+];
